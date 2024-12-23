bcf21ae00cb4dd4f0cffd4e30cbb0965ecb04869
Suraj Pratap <surajpratap@icloud.com>
2024-12-23 20:27:05 +0530

    added tests

--- src/app/__tests__/page.test.tsx
+++ src/app/__tests__/page.test.tsx
@@ -1,7 +1,6 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Root from "@/app/page";
-import Reports from "@/app/reports/page";
 
 describe('Page', () => {
   it('renders a heading', () => {
